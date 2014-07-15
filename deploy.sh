3b53244fc389265c2a7acd0ec3f17d375ceaa9c6
chris-mendoza <ccmendoza89@gmail.com>
2014-07-15 04:33:27 +0000

    reorg

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf /var/www/wikistorm
-cd /var/www && git clone git@github.com:chris-mendoza/wikistorm.git
-mkdir /var/www/wikistorm/config
-rsync -avz /var/www/config/* /var/www/wikistorm/config/
